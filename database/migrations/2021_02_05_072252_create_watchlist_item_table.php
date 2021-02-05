88a1939c1515a0ef5ab4fe626e1d0229199440ff
Anna Paajarvi <anna.paajarvi@chasacademy.se>
2021-02-05 09:46:14 +0100

    Add first migration draft. > > Co-authored-by: pernilla-hallgren pernilla.hallgren@chasacademy.se

--- database/migrations/2021_02_05_072252_create_watchlist_item_table.php
+++ database/migrations/2021_02_05_072252_create_watchlist_item_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateWatchlistItemTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,10 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('watchlist_item', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->boolean('role')->default(0);
-            $table->rememberToken();
+            $table->foreignId('movie_id')->constrained();
+            $table->foreignId('user_id')->constrained();
             $table->timestamps();
         });
     }
@@ -32,6 +28,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('watchlist_item');
     }
 }
