f17b518c7b56c7b3952f2b849b3cfa79d4f22314
Pernilla Hällgren <pernilla833@hotmail.com>
2021-02-09 09:06:47 +0100

    add blade movie form. > > Co-authored-by: Anna anna.paajarvi@chasacademy.se

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
