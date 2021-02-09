26fb6162d3e08fcafe4fc3ce4527d7f31477be59
EnzoBomark <enzo.bomark@chasacademy.se>
2021-02-09 11:18:54 +0100

    Revert changes

--- routes/web.php
+++ routes/web.php
@@ -17,8 +17,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-
-
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
