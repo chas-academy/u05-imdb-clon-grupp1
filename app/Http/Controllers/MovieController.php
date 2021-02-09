c577c869fe7cbba156945cc1840a6bde81131dbb
Oskar-Mikael <oskar.bostrom@chasacademy.se>
2021-02-09 14:56:57 +0100

    Scaffolding for controller methods

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -2,9 +2,52 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Genre;
+use App\Models\Movie;
+use App\Models\Review;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class MovieController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('movies.index');
+    }
+
+    public function show(Genre $genre, Movie $movie, Review $review, User $user)
+    {
+        return view('movies.show', compact('genre', 'movie', 'review', 'user'));
+    }
+
+    public function create()
+    {
+        return view('movies.create');
+    }
+
+    public function store()
+    {
+        return redirect('/movies');
+    }
+
+    public function edit($id)
+    {
+        $movie = Movie::findOrFail($id);
+
+        return view('movies.edit', compact('movie'));
+    }
+
+    public function update(Movie $movie)
+    {
+        return redirect("/movies/{$movie->id}");
+    }
+
+    public function destroy($id)
+    {
+        $movie = Movie::findOrFail($id);
+        $movie->delete();
+
+        return redirect('/movies');
+    }
+
 }
