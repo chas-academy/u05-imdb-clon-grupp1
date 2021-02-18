563916cc802d520959d7b5f9fd4ed5a34d721194
EnzoBomark <enzo.bomark@chasacademy.se>
2021-02-18 22:22:00 +0100

    Update genre buttons colors

--- app/View/Components/GenreButton.php
+++ app/View/Components/GenreButton.php
@@ -8,12 +8,12 @@ class GenreButton extends Component
 {
     public $link;
     public $color = array(
-    'bg-gradient-to-r from-green-300 to-blue-400',
-    'bg-gradient-to-r from-yellow-200 to-red-500',
-    'bg-gradient-to-r from-purple-200 to-purple-500',
-    'bg-gradient-to-r from-pink-200 to-pink-500',
-    'bg-gradient-to-r from-green-200 to-green-500',
-    'bg-indigo-900');
+    'bg-gradient-to-r from-green-300 to-blue-400 hover:from-green-400 hover:to-blue-500',
+    'bg-gradient-to-r from-yellow-200 to-red-500 hover:from-yellow-300 hover:to-red-600',
+    'bg-gradient-to-r from-purple-200 to-purple-500 hover:from-purple-300 hover:to-purple-600',
+    'bg-gradient-to-r from-pink-200 to-pink-500 hover:from-pink-300 hover:to-pink-600',
+    'bg-gradient-to-r from-green-200 to-green-500 hover:from-green-300 hover:to-green-600',
+    'bg-indigo-800 hover:bg-indigo-900');
 
     public $randomColor;
 
--- resources/views/components/genre-button.blade.php
+++ resources/views/components/genre-button.blade.php
@@ -1,6 +1,6 @@
 <div>
     <a href={{ $link }}>
-        <button class="{{ $randomColor() }} m-1 inline-block px-4 py-2 text-xs font-medium text-center text-white uppercase transition rounded-full shadow hover:shadow-xl hover:bg-gray-900 focus:outline-none">
+        <button class="{{ $randomColor() }} m-1 inline-block px-4 py-2 text-xs font-medium text-center text-white uppercase transition rounded-full shadow hover:shadow-xl focus:outline-none">
             {{ $slot }}
         </button>
     </a>
