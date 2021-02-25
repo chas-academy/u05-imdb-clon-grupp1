a944eb02b6ffc9fe582c8a12cf39e8539163e4b9
Oskar-Mikael <oskar.bostrom@chasacademy.se>
2021-02-25 20:49:30 +0100

    Create relation with Movies and Genres
    Co-authored-by: Simon Lindelöf <info@designproducers.se>

--- app/Models/Movie.php
+++ app/Models/Movie.php
@@ -13,7 +13,7 @@ class Movie extends Model
 
     public function genres()
     {
-        return $this->belongsToMany(Genre::class);
+        return $this->belongsToMany(Genre::class, 'genre_movies_pivot', 'movies_id', 'genres_id');
     }
 
     public function reviews()
