030ca2e440ef21fe4cab0cac8ca928d7edeeb6d8
Oskar-Mikael <oskar.bostrom@chasacademy.se>
2021-02-25 16:48:13 +0100

    Authentication for adding, editing and deleting review
    Co-authored-by: johanneslepsius <johannes.lepsius@chasacademy.se>
    Co-authored-by: Anna Paajarvi <anna.paajarvi@chasacademy.se>
    Co-authored-by: Pernilla Hällgren <pernilla833@hotmail.com>

--- resources/views/movies/show.blade.php
+++ resources/views/movies/show.blade.php
@@ -1,28 +1,33 @@
 <h1>Movies</h1>
 <a href="/movies">Back</a><br>
 
-    {{$movie->title}} 
-    {{$movie->description}} 
-    <br>
-
-{{--  På denna sida tänkt att man kan föra in reviews --}}
-<a href="/reviews/{{$movie->id}}/create">Add review</a><br>
+{{ $movie->title }}
+{{ $movie->description }}
 <br>
-@foreach($movie->reviews as $review)
-    {{$review->review}}<br>
-    {{$review->rating}}<br>
-    <form action="{{route('reviews.destroy', $review->id)}}" method="post">
-    @csrf
-    @method('DELETE')   
-    <button type="submit" name="delete">Delete review</button> 
-    </form>
+
+{{-- På denna sida tänkt att man kan föra in reviews --}}
+@if (Route::has('login'))
+    @auth
+        <a href="/reviews/{{ $movie->id }}/create">Add review</a><br>
+    @else
+        <p>Please <a href="/login">login</a> to add a review</p><br>
+    @endauth
+@endif
+
+@foreach ($movie->reviews as $review)
+    {{ $review->review }}<br>
+    {{ $review->rating }}<br>
+    @can('update', $review)
+    <a href="/reviews/{{ $review->id }}/edit">Edit review</a>
+    @endcan
+
+    @can('update', $review)
+        <form action="{{ route('reviews.destroy', $review->id) }}" method="post">
+            @csrf
+            @method('DELETE')
+            <button type="submit" name="delete">Delete
+
+                review</button>
+        </form>
+    @endcan
 @endforeach
-
-{{-- Skapa relation till reviews? --}}
-    {{-- @foreach ($movie->reviews as $review)
-        {{$review->review}}
-        {{$review->rating}}
-    @endforeach --}}
-    
-
-    
