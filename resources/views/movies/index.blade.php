fdb135be0a5313455e385a006c8b65dac717e0ff
Oskar-Mikael <oskar.bostrom@chasacademy.se>
2021-03-01 14:53:15 +0100

    Admin middleware for adding and editing movie

--- resources/views/movies/index.blade.php
+++ resources/views/movies/index.blade.php
@@ -1,11 +1,19 @@
 <h1>Movies</h1>
-<a href="/movies/create">Add movie</a><br>
+@auth
+    @if (Auth::user()->isRole() == 'admin')
+        <a href="/movies/create">Add movie</a><br>
+    @endif
+@endauth
 @foreach ($movies as $movie)
-    <a href="/movies/{{$movie->id}}">{{$movie->title}} </a>
-    <a href="/movies/{{ $movie->id }}/edit">Edit Movie</a>
-    <form method="POST" action="/movies/{{$movie->id}}">
-            @csrf
-            @method('DELETE')   
-            <button type="submit" name="delete">Delete movie</button> 
+    <a href="/movies/{{ $movie->id }}">{{ $movie->title }} </a>
+    @auth
+        @if (Auth::user()->isRole() == 'admin')
+            <a href="/movies/{{ $movie->id }}/edit">Edit Movie</a>
+        @endif
+    @endauth
+    <form method="POST" action="/movies/{{ $movie->id }}">
+        @csrf
+        @method('DELETE')
+        <button type="submit" name="delete">Delete movie</button>
     </form><br>
 @endforeach
