363d612ee4c5ccd13c442f64f21adfb95331d51d
Simon Lindelöf <info@designproducers.se>
2021-03-01 16:09:02 +0100

    fixed css again

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,6 +5,7 @@
  */
 
 require('./bootstrap');
+require('alpinejs');
 
 window.Vue = require('vue').default;
 
--- webpack.mix.js
+++ webpack.mix.js
@@ -13,4 +13,9 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
     .vue()
+    .postCss('resources/css/app.css', 'public/css', [
+        require('postcss-import'),
+        require('tailwindcss'),
+        require('autoprefixer'),
+    ])
     .sass('resources/sass/app.scss', 'public/css');
