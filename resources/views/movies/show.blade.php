cd9a00784ec0266cd197ad3f6e0d2afd6bbf6e77
Oskar-Mikael <oskar.bostrom@chasacademy.se>
2021-03-02 14:23:15 +0100

    Add watchlist button

--- resources/views/movies/show.blade.php
+++ resources/views/movies/show.blade.php
@@ -1,47 +1,44 @@
-<h1>{{$movie->title}}</h1>
-<a href="/movies">Back</a><br>
+<x-app-layout>
+    <h1>{{ $movie->title }}</h1>
+    <a href="/movies">Back</a><br>
 
-{{ $movie->title }}<br>
-{{ $movie->description }}
-<br>
+    {{ $movie->title }}<br>
+    {{ $movie->description }}
+    <br>
 
-<h4>Genres</h4>
-@foreach ($movie->genres as $genre)
-    {{$genre->name}}<br>
-@endforeach
+    <h4>Genres</h4>
+    @foreach ($movie->genres as $genre)
+        {{ $genre->name }}<br>
+    @endforeach
 
-@if (Route::has('login'))
-    @auth
-<form method="post" action="{{ route('movies.watchlist', $movie->id) }}">
-    @csrf
-    <button type="submit" name="delete">Add to watchlist
+    @if (Route::has('login'))
+        @auth
+            <div id="app">
+                <watchlist-button movie-id="{{ $movie->id }}" watchlist="{{ $watchlistStatus }}"></watchlist-button>
+            </div>
+            <a href="/reviews/{{ $movie->id }}/create">Add review</a><br>
+        @else
+            <p>Please <a href="/login">login</a> to add a review</p><br>
+        @endauth
+    @endif
+    @foreach ($reviews as $review)
+        <img src="{{ $review->user->profile->profileImage() }}" width="50px">
+        {{ $review->review }}<br>
+        {{ $review->rating }}<br>
+        By {{ $review->user->username }}<br>
+        @can('update', $review)
+            <a href="/reviews/{{ $review->id }}/edit">Edit review</a>
+        @endcan
 
-        </button>
-</form><br>
+        @can('update', $review)
+            <form action="{{ route('reviews.destroy', $review->id) }}" method="post">
+                @csrf
+                @method('DELETE')
+                <button type="submit" name="delete">Delete
 
-        <a href="/reviews/{{ $movie->id }}/create">Add review</a><br>
-    @else
-        <p>Please <a href="/login">login</a> to add a review</p><br>
-    @endauth
-@endif
-
-@foreach ($reviews as $review)
-<img src="{{$review->user->profile->profileImage()}}" width="50px">
-    {{ $review->review }}<br>
-    {{ $review->rating }}<br>
-    By {{$review->user->username}}<br>
-    @can('update', $review)
-    <a href="/reviews/{{ $review->id }}/edit">Edit review</a>
-    @endcan
-
-    @can('update', $review)
-        <form action="{{ route('reviews.destroy', $review->id) }}" method="post">
-            @csrf
-            @method('DELETE')
-            <button type="submit" name="delete">Delete
-
-                review</button>
-        </form>
-    @endcan
-@endforeach
-{!! $reviews->links() !!}
+                    review</button>
+            </form>
+        @endcan
+    @endforeach
+    {!! $reviews->links() !!}
+</x-app-layout>
