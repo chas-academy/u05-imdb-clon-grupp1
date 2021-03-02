44963bfa5c7b60bd45278f16783c6a66ccb086e2
Oskar-Mikael <oskar.bostrom@chasacademy.se>
2021-03-02 22:00:57 +0100

    Add vue components to blade

--- resources/views/movies/show.blade.php
+++ resources/views/movies/show.blade.php
@@ -13,15 +13,24 @@
 
     @if (Route::has('login'))
         @auth
-            <div id="app">
-                <watchlist-button movie-id="{{ $movie->id }}" watchlist="{{ $watchlistStatus }}"></watchlist-button>
-            </div>
-            <a href="/reviews/{{ $movie->id }}/create">Add review</a><br>
+
+            <watchlist-button movie-id="{{ $movie->id }}" watchlist="{{ $watchlistStatus }}"></watchlist-button>
+
+            {{-- <a href="/reviews/{{ $movie->id }}/create">Add review</a><br> --}}
+           
+              
+            <review-component user-id="{{auth()->user()->id}}" movie-id="{{ $movie->id }}"></review-component>
+            @if ($errors->has('review'))
+            <span class="text-red-600" role="alert">
+                <strong>{{ $errors->first('review') }}</strong>
+            </span>
+            @endif
+            
         @else
             <p>Please <a href="/login">login</a> to add a review</p><br>
         @endauth
     @endif
-    @foreach ($reviews as $review)
+    {{-- {{-- @foreach ($reviews as $review)
         <img src="{{ $review->user->profile->profileImage() }}" width="50px">
         {{ $review->review }}<br>
         {{ $review->rating }}<br>
@@ -31,14 +40,15 @@
         @endcan
 
         @can('update', $review)
-            <form action="{{ route('reviews.destroy', $review->id) }}" method="post">
+            {{-- <form action="{{ route('reviews.destroy', $review->id) }}" method="post">
                 @csrf
                 @method('DELETE')
                 <button type="submit" name="delete">Delete
 
                     review</button>
-            </form>
+            </form> 
         @endcan
     @endforeach
-    {!! $reviews->links() !!}
+    {!! $reviews->links() !!} --}}
+    <review-list movie-id="{{ $movie->id }}"></review-list>
 </x-app-layout>
