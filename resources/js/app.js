4e0bb2b60625bb4a3318a9978d416f98a621c7ba
Oskar-Mikael <oskar.bostrom@chasacademy.se>
2021-03-02 22:00:10 +0100

    Add components

--- resources/js/app.js
+++ resources/js/app.js
@@ -23,6 +23,8 @@ window.Vue = require('vue').default;
 Vue.component('watchlist-button', require('./components/WatchlistButton.vue').default);
 Vue.component('example-component', require('./components/ExampleComponent.vue').default);
 Vue.component('moviestar-component', require('./components/MovieStarComponent.vue').default);
+Vue.component('review-component', require('./components/ReviewComponent.vue').default);
+Vue.component('review-list', require('./components/ReviewListComponent.vue').default);
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
