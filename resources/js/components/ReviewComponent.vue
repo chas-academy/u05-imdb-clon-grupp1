fe371354f4d4921707a8806cedbef3b7baf759d4
Oskar-Mikael <oskar.bostrom@chasacademy.se>
2021-03-02 21:58:19 +0100

    Initial commit

---
+++ resources/js/components/ReviewComponent.vue
@@ -0,0 +1,60 @@
+<template>
+    
+        <form method="post" @submit.prevent="addReview">
+        <label for="title">Review</label>
+
+        <input type="text" v-model="review" name="review" value=""><br>
+     
+
+        <!-- <select name="rating">
+            <option value="" hidden>Select a rating</option>
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+        </select> -->
+      
+       
+        
+        <button type="submit">Add review</button>
+        </form>
+</template>
+
+<script>
+import MovieStarComponent from './MovieStarComponent';
+
+export default {
+    props: ['movieId', 'userId'],
+    components: {
+        MovieStarComponent
+    },
+
+    data() {
+        return {
+            review: '',
+            movies_id: this.movieId,
+            user_id: this.userId,
+        }
+    },
+
+    methods: {
+        addReview() {
+            axios.post('/review-api', {
+                review:this.review,
+                movies_id: this.movies_id,
+                user_id: this.user_id,
+                })
+            .then(response => {
+                console.log(response)
+            })
+            
+        }
+    }
+}
+</script>
+
+<style>
+
+        
+</style>
---
+++ resources/js/components/ReviewListComponent.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+      <h2>Reviews</h2>
+        <p v-for="review in reviews" :key="review.id"> {{review.review}} </p>
+  </div>
+</template>
+
+<script>
+export default {
+    props: ['movieId'],
+    data() {
+        return {
+            reviews: []
+        }
+    },
+    created() {
+        axios.get('/movies/' + this.movieId)
+        .then(response => 
+            this.reviews = response.data);
+          
+        }
+    }
+
+</script>
+
+<style>
+
+</style>
