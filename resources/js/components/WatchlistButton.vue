08aa45d7c703414af44e319b4acbdf91b7fe190d
Oskar-Mikael <oskar.bostrom@chasacademy.se>
2021-03-02 14:23:29 +0100

    Finish watchlist button component

--- resources/js/components/WatchlistButton.vue
+++ resources/js/components/WatchlistButton.vue
@@ -1,13 +1,43 @@
 <template>
-  
+  <div>
+    <button @click="addToWatchlist" v-text="buttonText"></button>
+    <img
+      class="bookmark"
+      @click="addToWatchlist"
+      v-bind:src="'/storage/' + img_path"
+    />
+  </div>
 </template>
 
 <script>
 export default {
+  props: ["movieId", "watchlist"],
 
-}
+  data: function () {
+    return { status: this.watchlist };
+  },
+  methods: {
+    addToWatchlist() {
+      axios.post("/watchlist/" + this.movieId).then((response) => {
+        console.log(response.data);
+        this.status = !this.status;
+      });
+    },
+  },
+  computed: {
+    buttonText() {
+      return this.status ? "Remove from Watchlist" : "Add to Watchlist";
+    },
+
+    img_path() {
+      return this.status ? "bookmark_filled.png" : "bookmark.png";
+    },
+  },
+};
 </script>
 
-<style>
-
+<style scoped>
+.bookmark {
+  cursor: pointer;
+}
 </style>
