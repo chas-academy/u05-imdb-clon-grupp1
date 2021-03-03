ed466ca3d4a77ae41073641106c2baff6154bea4
Oskar-Mikael <oskar.bostrom@chasacademy.se>
2021-03-03 15:43:38 +0100

    Add vue events

--- resources/js/app.js
+++ resources/js/app.js
@@ -8,6 +8,7 @@ require('./bootstrap');
 require('alpinejs');
 
 window.Vue = require('vue').default;
+window.Event = new Vue();
 
 /**
  * The following block of code may be used to automatically register your
