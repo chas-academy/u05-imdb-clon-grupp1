43f64773633b15fcb000c3a6b4a3aeed83b99da5
Oskar-Mikael <oskar.bostrom@chasacademy.se>
2021-03-03 15:43:10 +0100

    Display reviews related to the movieid

--- resources/js/components/ReviewListComponent.vue
+++ resources/js/components/ReviewListComponent.vue
@@ -1,7 +1,14 @@
 <template>
   <div>
-      <h2>Reviews</h2>
-        <p v-for="review in reviews" :key="review.id"> {{review.review}} </p>
+      <h2 class="mb-8">Reviews</h2>
+      <div v-for="review in reviews" :key="review.id">
+          <div v-bind="movieId" v-if="movieId == review.movies_id">
+        <p> {{review.review}} </p>
+        <p>Score: {{review.rating}}</p>
+        <p class="mb-4"><a v-bind:href="'/profile/' + review.user_id">By {{review.user.username}}</a></p>
+          </div>
+        </div>
+        
   </div>
 </template>
 
@@ -14,10 +21,15 @@ export default {
         }
     },
     created() {
-        axios.get('/movies/' + this.movieId)
-        .then(response => 
-            this.reviews = response.data);
-          
+        axios.get('/review')
+        .then(response => {
+            this.reviews = response.data.reviews
+            });
+            
+          Event.$on('reviewAdded', (review, rating, id) => {
+            this.reviews.push(review, rating, id);
+
+        })
         }
     }
 
