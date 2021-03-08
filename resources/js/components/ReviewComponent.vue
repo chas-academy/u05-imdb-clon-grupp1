750802a1d395984b42a56ad8ae7e6ee123fa5a6a
Oskar-Mikael <oskar.bostrom@chasacademy.se>
2021-03-08 17:10:50 +0100

    Formatting

--- resources/js/components/ReviewComponent.vue
+++ resources/js/components/ReviewComponent.vue
@@ -1,12 +1,11 @@
 <template>
-    <div>
-        <form method="post" @submit.prevent="addReview">
-        <label for="title">Review</label>
+  <div>
+    <form method="post" @submit.prevent="addReview">
+      <label for="title">Review</label>
 
-        <input type="text" v-model="review" name="review" value=""><br>
-     
+      <input type="text" v-model="review" name="review" value="" /><br />
 
-        <!-- <select name="rating">
+      <!-- <select name="rating">
             <option value="" hidden>Select a rating</option>
             <option value="1">1</option>
             <option value="2">2</option>
@@ -14,63 +13,56 @@
             <option value="4">4</option>
             <option value="5">5</option>
         </select> -->
-      
-       
-        
-        <button type="submit">Add review</button>
-        </form>
-        <p v-if="errors.length">
-            <b v-for="error in errors" :key="error.id">
-                {{error}}
-            </b>
-        </p>
-        </div>
+
+      <button type="submit">Add review</button>
+    </form>
+    <p v-if="errors.length">
+      <b class="text-red-600" v-for="error in errors" :key="error.id">
+        {{ error }}
+      </b>
+    </p>
+  </div>
 </template>
 
 <script>
-import MovieStarComponent from './MovieStarComponent';
+import MovieStarComponent from "./MovieStarComponent";
 
 export default {
-    props: ['movieId', 'userId'],
-    components: {
-        MovieStarComponent
-    },
+  props: ["movieId", "userId"],
+  components: {
+    MovieStarComponent,
+  },
 
-    data() {
-        return {
-            review: '',
-            movies_id: this.movieId,
-            user_id: this.userId,
-            errors: '',
-        }
-    },
+  data() {
+    return {
+      review: "",
+      movies_id: this.movieId,
+      user_id: this.userId,
+      errors: "",
+    };
+  },
 
-    methods: {
-        addReview() {
-            this.errors = [];
-            if (!this.review) {
-            this.errors.push('Please write a review.');
+  methods: {
+    addReview() {
+      this.errors = [];
+      if (!this.review) {
+        this.errors.push("Please write a review.");
       }
-            axios.post('/review-api', {
-                review:this.review,
-                movies_id: this.movies_id,
-                user_id: this.user_id,
-                })
-            .then(response => {
-                console.log(response)
-                this.review = ''
-            })
-            Event.$emit('reviewAdded', {
-                review:this.review,
-                movies_id: this.movies_id,
-                user_id: this.user_id,
-                })
-        }
-    }
-}
+      axios.post("/review-api", {
+        review: this.review,
+        movies_id: this.movies_id,
+        user_id: this.user_id,
+      })
+      
+      .then((response) => {
+        console.log(response);
+        this.review = "";
+        alert("Review added");
+      });
+    },
+  },
+};
 </script>
 
 <style>
-
-        
 </style>
