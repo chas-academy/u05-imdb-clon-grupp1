4b9c00e043331868b9b6ef3208635928a2bf4e50
Anna Paajarvi <anna.paajarvi@chasacademy.se>
2021-03-10 08:04:38 +0100

    Add style template blade. > > Co-authored-by: Pernilla pernilla.hallgren@chasacademy.se
    Co-authored-by: Emanuel emanuel.popa@chasacademy.se>

---
+++ resources/views/styles.blade.php
@@ -0,0 +1,28 @@
+<!--   review cards  -->
+<div class="max-w-sm w-full lg:max-w-full lg:flex">
+    <div class=" bg-indigo-400 rounded-lg  p-4 flex flex-col justify-between leading-normal">
+        <div class="mb-8">
+            <p class="text-gray-700 text-base">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.</p>
+        </div>
+        <div class="flex items-center">
+            <img class="w-10 h-10 rounded-full mr-4" src="/storage/images/avatar.jpg" alt="bild">
+            <div class="text-sm">
+                <p class="text-gray-900 leading-none">Reviwer Name</p>
+                <p class="text-gray-600">Created at:</p>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!--  button list: just change color -->
+<button class="bg-blue-500 hover:bg-blue-700 text-white font-light py-2 px-4 rounded-lg">
+All btn except genres
+</button>
+
+<button class="bg-blue-500 hover:bg-blue-700 text-white font-light  py-2 px-4 rounded-full">
+Genre Button
+</button>
+
+<!-- classes for form (label and input) and searchfield -->
+<label for="" class="text-white font-medium">input</label>
+<input type="text" class="bg-gray-800 rounded-full w-64 px-4 pl-8 py-1 font-normal focus:outline-none focus:shadow-outline" placeholder="class for input">
