e18b5ce4d051d65947d4d96bdf134a2143525d3c
EnzoBomark <enzo.bomark@chasacademy.se>
2021-03-14 19:10:22 +0100

    Add slider filters

--- resources/js/components/MovieSlider.vue
+++ resources/js/components/MovieSlider.vue
@@ -1,17 +1,31 @@
 <template>
-    <div>
-        <flicking
+    <div class="flex flex-col justify-center items-center" >
+        <div>
+            <form action="/action_page.php">
+            <input type="radio" id="male" name="gender" value="male" v-on:click="scoreMovies(moviesList)">
+            <label for="male">Score</label>
+            <input type="radio" id="female" name="gender" value="female" v-on:click="newMovies(moviesList)" checked>
+            <label for="female">New</label>
+            <input type="radio" id="other" name="gender" value="other" v-on:click="commingMovies(moviesList)">
+            <label for="other">Comming</label>
+            </form>
+        </div>
+
+        <div class="w-screen">
+            <flicking
             class="h-80 my-5"
-            :options="{ circular: true, moveType: 'freeScroll'}"
+            :options="{ circular: true, moveType: 'freeScroll', gap: 15}"
 
             @select="e => user(e)">
 
-                <div v-for="(movie, index) in movies" :key="movie.id" class="h-80 w-56 p-2 relative panel text-white">
+                <div v-for="(movie, index) in moviesPrint" :key="movie.id" class="h-80 w-56 relative panel text-white">
                     <img  :src="'/storage/' + movie.img_path" class="h-full w-full rounded-3xl cursor-pointer object-cover">
                     <button class="absolute top-5 right-5 rounded-full bg-white w-6 h-6 text-gray-900 transform hover:scale-110 hover:opacity-80">{{movie.id}}</button>
                 </div>
 
-        </flicking>
+            </flicking>
+        </div>
+
     </div>
 </template>
 
@@ -23,13 +37,18 @@
         },
         data() {
             return {
-                movies: this.movies,
+                moviesPrint: this.moviesPrint,
+                moviesList: this.movieslist,
             };
         },
-        mounted() {
+        beforeMount(){
             axios.get('/movie-api')
-            .then(response => this.movies = response.data.data.slice().reverse())
-
+            .then(response => {
+                this.moviesPrint = response.data.data.slice().reverse().slice(0,20);
+                this.moviesList = response.data.data;
+                })
+        },
+        mounted() {
             if (localStorage.getItem('reloaded')) {
                 localStorage.removeItem('reloaded');
             } else {
@@ -38,7 +57,10 @@
             }
         },
         methods: {
-            user: function (e) { window.location.assign("/movies/" + this.movies[e.index].id) }
+            user: function (e) { window.location.assign("/movies/" + this.movies[e.index].id) },
+            scoreMovies: function (e) { this.moviesPrint = e.sort((a,b) => a.top_rating - b.top_rating).slice().reverse().slice(0,20) },
+            newMovies: function (e) { this.moviesPrint = e.sort((a,b) => a.id - b.id).slice().reverse().slice(0,20)},
+            commingMovies: function (e) { this.moviesPrint = e.filter(movie =>  movie.release_date >= new Date().getFullYear()).slice().reverse().slice(0,20) }
         }
     };
 </script>
