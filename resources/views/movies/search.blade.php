c358205fb01b2cc7532bc71d268d022eb66e05e0
Popa <emapopa1995@gmail.com>
2021-03-24 10:56:09 +0100

    Aded save function in the search field

--- resources/views/movies/search.blade.php
+++ resources/views/movies/search.blade.php
@@ -5,7 +5,7 @@
     <div class="flex flex-col justify-center items-center">
         <h1 class="text-3xl font-medium p-7">Find, save and review movies</h1>
         <form class="w-4/5 max-w-screen-xl relative" action="{{route('search')}}" method="GET">
-            <input type="text" class="bg-gray-800 w-full rounded-2xl px-6 py-4 pl-4 font-bold text-lg focus:outline-none shadow-lg border-l-0 border-b-0 border-gray-700 border-opacity-50" placeholder="Search movies..." value="" name="query" id="query">
+            <input type="text" class="bg-gray-800 w-full rounded-2xl px-6 py-4 pl-4 font-bold text-lg focus:outline-none shadow-lg border-l-0 border-b-0 border-gray-700 border-opacity-50" placeholder="Search movies..." value="{{request()->input('query')}}" name="query" id="query">
             <button>
                 <x-search-icon/>
             </button>
