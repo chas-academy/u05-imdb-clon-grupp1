f77fe0960588e4b7c7b56a07e180fc95b3e263cb
EnzoBomark <enzo.bomark@chasacademy.se>
2021-03-24 09:50:56 +0100

    Show latest
    Co-Authored-By: pernilla hällgren <70695214+pernilla-hallgren@users.noreply.github.com>
    Co-Authored-By: apaajarvi <70699418+apaajarvi@users.noreply.github.com>

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -27,7 +27,7 @@ class MovieController extends Controller
     {
         $watchlistStatus = (auth()->user() ? auth()->user()->profile->movies->contains($movie->id) : false);
 
-        $reviews = $movie->reviews()->paginate(4);
+        $reviews = $movie->reviews()->latest()->paginate(4);
         return view('movies.show', compact('movie', 'reviews', 'user', 'watchlistStatus'));
     }
 
